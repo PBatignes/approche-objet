3d74d9bfe11a9d56e4c868c076ac2dbcb14359d2
Pierre BATIGNES <batignesp@gmail.com>
2020-03-17 16:58:07 +0100

    Tp test unitaire

---
+++ src/main/java/utils/IncorrectParameterException.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package utils;
+
+/**
+ *
+ * @author Pierre
+ *
+ */
+public class IncorrectParameterException extends Exception {
+
+	public IncorrectParameterException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/test/java/StringUtilsTest.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import utils.IncorrectParameterException;
+import utils.StringUtils;
+
+/**
+ * 
+ */
+
+/**
+ *
+ * @author Pierre
+ *
+ */
+public class StringUtilsTest {
+	
+	@Test
+	public void levenshteinDistance1() {
+		
+		try {
+			int res = StringUtils.levenshteinDistance("java", "jva");
+			assertEquals(1, res);
+		} catch (IncorrectParameterException e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void levenshteinDistance2() {
+		
+		try {
+			int res = StringUtils.levenshteinDistance("jva", "java");
+			assertEquals(1, res);
+		} catch (IncorrectParameterException e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void levenshteinDistance3() {
+		
+		try {
+			int res = StringUtils.levenshteinDistance("java", "jeva");
+			assertEquals(1, res);
+		} catch (IncorrectParameterException e) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void levenshteinDistance4() {
+		
+		try {
+			int res = StringUtils.levenshteinDistance("", "java");
+			assertEquals(4, res);
+		} catch (IncorrectParameterException e) {
+			fail();
+		}
+	}
+	
+	@Test(expected = IncorrectParameterException.class)
+	public void levenshteinDistance5() throws IncorrectParameterException {
+		
+		try {
+			int res = StringUtils.levenshteinDistance(null, "java");
+			assertEquals(1, res);
+		} catch (IncorrectParameterException e) {
+			throw e;
+		}
+	}
+
+}
+
